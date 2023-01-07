9480647d85a4d803a0746d36e90567ccf9c93516
Dev7Eng4 <duongbich9n7@gmail.com>
2023-01-07 14:44:19 +0700

    1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React hello b commit 1 commit 2 commit 3
+          Learn React hello b commit 1 commit 2 commit 3 oh
         </a>
       </header>
     </div>
