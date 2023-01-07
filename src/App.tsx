b32f3cae0266e2604f04187d8b88469a0f73d3dd
Dev7Eng4 <duongbich9n7@gmail.com>
2023-01-07 14:19:34 +0700

    commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Learn React hello b
+          Learn React hello b commit 1
         </a>
       </header>
     </div>
